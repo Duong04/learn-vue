09bbd0371bc2c145d15c150c72f3f0bc7e9448d5
Duong <tinhabc3009@gmail.com>
2024-05-28 00:02:40 +0700

    #2 - Learn v-if, v-show, v-for, etc...

--- src/App.vue
+++ src/App.vue
@@ -6,24 +6,54 @@
   const count = ref(0);
   const id = 'button-id';
 
-  const isCheck = false;
+  const isCheck = ref(false);
 
   const bidings = {
     id: 'button-id',
     class: 'btn',
     style: {
       backgroundColor: 'green',
-      color: 'blue'
+      color: '#fff',
+      'border': 'none'
     }
   }
+
+  const myCourse = ref([
+    {
+      name: 'My Course 1',
+      price: 100
+    },
+    {
+      name: 'My Course 2',
+      price: 90
+    },
+    {
+      name: 'My Course 3',
+      price: 50
+    },
+    {
+      name: 'My Course 4',
+      price: 70
+    },
+    {
+      name: 'My Course 5',
+      price: 90
+    },
+  ])
+
 </script>
 
+
 <template>
-    <h1>Hello world!</h1>
+    <h1 v-show="isCheck">Hello world!</h1>
+    <h1 v-if="isCheck">Hello world 2!</h1>
     <h3>Nguyễn Thành {{name}}</h3>
     <h5>Color is <span v-html="rawHtml"></span></h5>
-    <button :disabled="isCheck" v-bind="bidings" @click="count++">Click</button>
-    <h1>{{count}}</h1>
+    <div v-if="myCourse.length >= 0" v-for="(item, index) in myCourse" :key="index">
+      <h1>{{item.name}}</h1>
+      <h5>{{item.price}}</h5>
+    </div>
+    <button v-bind="bidings" @click="isCheck = !isCheck">Click</button>
 </template>
 
 <style scoped>
