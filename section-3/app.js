499e715dc83b25561a787fdd32e7e5d0144ba6b1
Duong <tinhabc3009@gmail.com>
2024-08-21 22:47:40 +0700

    #3 - Section 3

---
+++ section-3/app.js
@@ -0,0 +1,31 @@
+const app = Vue.createApp({
+  data() {
+    return {
+      counter: 0,
+      name: 'Name'
+    };
+  },
+  computed: {
+    outputFullname() {
+      console.log('okok');
+      return this.name;
+    },
+  },
+  methods: {
+    setName(event) {
+      this.name = event.target.value;
+    },
+    add(num) {
+      this.counter = this.counter + num;
+    },
+    reduce(num) {
+      this.counter = this.counter - num;
+      // this.counter--;
+    },
+    resetInput() {
+      this.name = '';
+    }
+  }
+});
+
+app.mount('#events');
