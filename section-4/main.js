8e00bd5774de8cce656e2ba24193a1d360e91953
Duong <tinhabc3009@gmail.com>
2024-08-22 11:33:04 +0700

    #4 - Section 4

---
+++ section-4/main.js
@@ -0,0 +1,13 @@
+Vue.createApp({
+    data() {
+        return {
+            students: {
+                name: "Nguyễn Thành Đường",
+                age: 19
+            },
+            image: "https://i.pinimg.com/236x/5e/02/88/5e0288ee3d881376a7ded12ce56c7dea.jpg",
+            linkHref: "https://duong04.online",
+        }
+    }
+}).mount('#app');
+
