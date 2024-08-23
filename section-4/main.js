acb325c85255a8ec3af39d1a33fd0a5d1062afd8
Duong <tinhabc3009@gmail.com>
2024-08-23 23:10:26 +0700

    #4 - Section 4

--- section-4/main.js
+++ section-4/main.js
@@ -7,6 +7,26 @@ Vue.createApp({
             },
             image: "https://i.pinimg.com/236x/5e/02/88/5e0288ee3d881376a7ded12ce56c7dea.jpg",
             linkHref: "https://duong04.online",
+            message: "<h1>Hello world!</h1>",
+            count: 1
+        }
+    },
+    methods: {
+        randoms() {
+            return Math.floor(Math.random() * 6) +1;
+        },
+        render() {
+            const arr = [this.randoms(), this.randoms(), this.randoms()];
+            const score = arr.reduce((total, item) => total += item, 0);
+
+            if (score > 10) {
+                return 'Tài';
+            }else {
+                return "Xỉu"
+            }
+        },
+        handleClick() {
+            this.count.value++;
         }
     }
 }).mount('#app');
